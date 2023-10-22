a3a533fbdbf100f919c85877c20b803082abc445
Raj Tupe <114012248+rajtupe987@users.noreply.github.com>
2023-10-22 08:04:00 +0530

    done for day-2

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,25 +1,29 @@
-const express=require("express");
+const express = require("express");
 
-const userRoute=express.Router();
-const nodemailer=require("nodemailer");
+const bcrypt = require("bcrypt");
 
-const bcrypt=require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const {userModel}=require("../model/user.model");
+const userRoute = express.Router();
+
+const nodemailer = require("nodemailer");
+
+
+const { userModel } = require("../model/user.model");
 
 require("dotenv").config()
 
 // for specifying emailer service and authentication for sender
-const transporter =nodemailer.createTransport({
-  service:"gmail",
-   auth:{
-     user:process.env.mail,
-     pass:process.env.pass_for_mail
-   }
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.mail,
+    pass: process.env.pass_for_mail
+  }
 
 })
 
- 
+
 
 userRoute.post("/signup", async (req, res) => {
   const { username, email, password } = req.body;
@@ -63,18 +67,35 @@ userRoute.post("/signup", async (req, res) => {
 });
 
 
-// for user authentication purpose
-userRoute.post("/signin",(res,req)=>{
- 
-
+userRoute.post("/login", async (req, res) => {
   try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email:email });
+    if (!user) {
+      return res.status(401).json({ msg: "User with this email not found", ok: false })
+    }
+    const isPasswordSame = await bcrypt.compare(password, user.password)
     
+    if (!isPasswordSame) {
+      return res.status(401).json({ msg: "Invalid email or password", ok: false })
+    }
+
+    //{ userId: user._id } == this is going to encoded into jwt
+    const token = jwt.sign({ userId: user._id }, process.env.secreat_key, { expiresIn: '1hr' })
+    //const refreshToken = jwt.sign({ userId: user._id }, process.env.refresh_secret, { expiresIn: "3hr" })
+    const response = {
+      "ok": true,
+      "token": token
+    }
+
+    res.status(200).json(response)
   } catch (error) {
-    
+    res.status(400).json({ "ok": false, "msg": error.message });
   }
-  
 })
 
-module.exports={
-     userRoute
+
+
+module.exports = {
+  userRoute
 }
